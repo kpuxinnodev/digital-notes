383f7905969d891344be90ce4234b8cf30c513f2
kpuxinnodev <kpuxinno.dev@gmail.com>
2024-08-07 11:07:09 -0300

    iconos reactivos en nav, faq y guia agregados

--- Front-End/src/components/NavBar.vue
+++ Front-End/src/components/NavBar.vue
@@ -1,81 +1,114 @@
 <template>
-    <div class="principal">
-        <v-card>
-        <v-layout>
-          <v-navigation-drawer app
-            permanent
-            absolute
-            location="left"
-            rail
-          >
+  <div class="principal">
+    <v-card>
+      <v-layout>
+        <v-navigation-drawer app permanent absolute location="left" rail>
+          <v-list>
+            <img src="/img/nav/user.png" alt="logo.png" />
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <div class="navegador">
             <v-list>
-                <img src="/img/nav/user.png" alt="logo.png">
+              <router-link
+                v-for="(ruta, index) in rutas_uno"
+                :key="index"
+                :to="ruta.path"
+                :class="{ 'active-link': isActive(ruta.path) }"
+              >
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon :class="iconClass(ruta.path)">
+                      {{ ruta.icon }}
+                    </v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </router-link>
             </v-list>
 
-            <v-divider></v-divider>
+            <v-list>
+              <router-link
+                v-for="(rutas, index) in rutas_dos"
+                :key="index"
+                :to="rutas.path"
+                :class="{ 'active-link': isActive(rutas.path) }"
+              >
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon :class="iconClass(rutas.path)">
+                      {{ rutas.icon }}
+                    </v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </router-link>
+            </v-list>
+          </div>
+        </v-navigation-drawer>
 
-            <div class="navegador">
-                <v-list>
-                    <router-link v-for="(ruta, index) in rutas_uno" :key="index" :to="ruta.path">
-                        <v-list-item :prepend-icon="ruta.icon" :title="ruta.title" :value="ruta.value"></v-list-item>
-                    </router-link>
-                </v-list>
-
-                <v-list>
-                    <router-link v-for="(rutas, index) in rutas_dos" :key="index" :to="rutas.path">
-                        <v-list-item :prepend-icon="rutas.icon" :title="rutas.title" :value="rutas.value"></v-list-item>
-                    </router-link>
-                </v-list>
-            </div>
-            </v-navigation-drawer>
-
-          <v-main style="height: 100vh"></v-main>
-        </v-layout>
-      </v-card>
-    </div>
+        <v-main style="height: 100vh"></v-main>
+      </v-layout>
+    </v-card>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 
-const router = useRouter()
-const rutas_uno = ref([ //Menús principales
-  { path: '/miespacio', title: 'Mi Espacio', value: 'miespacio', icon: 'mdi-account'},
-  { path: '/grupo', title: 'Grupo', value: 'grupo', icon: 'mdi-account-multiple'},
+const route = useRoute()
+const rutas_uno = ref([ // Menús principales
+  { path: '/miespacio', icon: 'mdi-account' },
+  { path: '/grupo', icon: 'mdi-account-multiple' },
 ])
-const rutas_dos = ref([ //Menús secundarios
-  { path: '/notificaciones', title: 'Notificaciones', value: 'notificaciones', icon: 'mdi-bell'},
-  { path: '/faq', title: 'FAQ', value: 'faq', icon: 'mdi-frequently-asked-questions'},
-  { path: '/guia', title: 'Guía', value: 'guia', icon: 'mdi-book'},
-  { path: '/configuracion', title: 'Configuración', value: 'configuracion', icon: 'mdi-cog'},
-  { path: '/salir', title: 'Salir', value: 'salir', icon: 'mdi-exit-run'}
+const rutas_dos = ref([ // Menús secundarios
+  { path: '/notificaciones', icon: 'mdi-bell' },
+  { path: '/faq', icon: 'mdi-frequently-asked-questions' },
+  { path: '/guia', icon: 'mdi-book' },
+  { path: '/configuracion', icon: 'mdi-cog' },
+  { path: '/salir', icon: 'mdi-exit-run' }
 ])
+
+const isActive = (path) => {
+  return route.path === path
+}
+
+const iconClass = (path) => {
+  return isActive(path) ? 'active-icon' : 'inactive-icon'
+}
 </script>
 
 <style>
-
-*, ::before, ::after {
-  color: white;
+*,
+::before,
+::after {
   text-decoration: none;
 }
 
-img{
+img {
   height: 53px;
 }
 
 .navegador {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 88%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 88%;
 }
 
-.v-navigation-drawer__content{
+.v-navigation-drawer__content {
   overflow-y: hidden !important;
 }
 
 .v-list {
-    padding: 0;
+  padding: 0;
+}
+
+.active-icon {
+  color: blue; /* Cambia esto al color azul que desees para el ícono activo */
+}
+
+.inactive-icon {
+  color: white; /* Cambia esto al color blanco que desees para los íconos inactivos */
 }
 </style>
