764b15aa404174e3378ff982700c012295ae5259
Tinmanya2006 <robervalentinms2006@gmail.com>
2024-10-23 21:58:50 -0300

    Solucion de errores

--- src/router/index.js
+++ src/router/index.js
@@ -98,10 +98,6 @@ router.beforeEach((to, from, next) => {
     next();
   }
 });
-<<<<<<< HEAD
-//Nuevas cosas
-=======
->>>>>>> master
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {
